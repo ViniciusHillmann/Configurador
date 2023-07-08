66ce52fea4d26a8b052ca2c2320cfc58599e1a44
ViniciusHillmann <viniciushillmann@gmail.com>
2023-07-07 23:04:55 -0300

    crud feito

--- db.js
+++ db.js
@@ -7,47 +7,159 @@ const mysql = require("mysql2/promise");
 // toda vez que usar. 
 const client = mysql.createPool(process.env.CONNECTION_STRING);
 
-
-
 // função deve ser assincrona  bem como deve-se utilizar o await na solicitação ao banco
-async function selectCustomers(){
+async function selectMps(){
     // armazena em results a consulta feita no banco de todos os clientes cadastrados.
-    const results = await client.query("SELECT * FROM clientes;");
+    const results = await client.query("SELECT * FROM materiaprima;");
     // deve-se utlizar results[0], pois caso contrário a função retorna outros metadados.
     return results[0];
 }
 
 // função que retorna somente um cliente listado pelo id
-async function selectCustomer(id){
-    const results = await client.query("SELECT * FROM clientes WHERE id=?;",[id]);
+async function selectMp(id){
+    const results = await client.query("SELECT * FROM materiaprima WHERE idMp=?;",[id]);
     return results[0];
 }
 
-// função para inserir um novo cadastro no banco de dados
-async function insertCustomer(customer){
+// função deve ser assincrona  bem como deve-se utilizar o await na solicitação ao banco
+async function selectGrs(){
+    // armazena em results a consulta feita no banco de todos os clientes cadastrados.
+    const results = await client.query("SELECT * FROM grades;");
+    // deve-se utlizar results[0], pois caso contrário a função retorna outros metadados.
+    return results[0];
+}
+
+// função que retorna somente um cliente listado pelo id
+async function selectGr(id){
+    const results = await client.query("SELECT * FROM grades WHERE idGr=?;",[id]);
+    return results[0];
+}
+
+// função deve ser assincrona  bem como deve-se utilizar o await na solicitação ao banco
+async function selectEqs(){
+    // armazena em results a consulta feita no banco de todos os clientes cadastrados.
+    const results = await client.query("SELECT * FROM produtofinal;");
+    // deve-se utlizar results[0], pois caso contrário a função retorna outros metadados.
+    return results[0];
+}
+
+// função que retorna somente um cliente listado pelo id
+async function selectEq(id){
+    const results = await client.query("SELECT * FROM produtofinal WHERE idEq=?;",[id]);
+    return results[0];
+}
+
+// retorna uma estrutura do GR selecionado
+async function selectEstruturaGr(idGr){
+    const results = await client.query("SELECT * FROM estruturagr WHERE idGr=?;",[idGr]);
+    return results[0];
+}
+
+// retorna uma estrutura do equipamento selecionado
+async function selectEstruturaEq(idEq){
+    const results = await client.query("SELECT * FROM estruturaeq WHERE idEq=?;",[idEq]);
+    return results[0];
+}
+
+// retorna uma estrutura do equipamento selecionado
+async function selectTipoProdutos(){
+    const results = await client.query("SELECT * FROM tipoproduto;");
+    return results[0];
+}
+
+// retorna uma estrutura do equipamento selecionado
+async function selectTipoProduto(id){
+    const results = await client.query("SELECT * FROM tipoproduto WHERE id=?;",[id]);
+    return results[0];
+}
+
+// retorna uma estrutura do equipamento selecionado
+async function selectProdutoNiveis(){
+    const results = await client.query("SELECT * FROM produtonivel;");
+    return results[0];
+}
+
+// retorna uma estrutura do equipamento selecionado
+async function selectProdutoNivel(idTipoProduto){
+    const results = await client.query("SELECT * FROM produtonivel WHERE idTipoProduto=?;",[idTipoProduto]);
+    return results[0];
+}
+
+// função para inserir um novo cadastro na tabela de mp
+async function insertMp(mp){
     // cria uma variavel para armazenar os valores que serão passados por parametro para o banco de dados cadastrar.
-    const values = [customer.nome, customer.idade, customer.uf];
+    const values = [mp.codMp, mp.descMp, mp.custoMp];
     // armazena no banco de dados as informações do novo usuário cadastrado.
-    await client.query("INSERT INTO clientes(nome,idade,uf) VALUES (?,?,?);", values); 
+    await client.query("INSERT INTO materiaprima(codMp,descMp,custoMp) VALUES (?,?,?);", values); 
 }
 
-// função para atualizar/ editar um cliente no banco de dados
-async function updateCustomer(id, customer){
-    const values = [customer.nome, customer.idade, customer.uf, id];
-    await client.query("UPDATE clientes SET nome=?,idade=?,uf=? WHERE id=?;", values); 
+// função para inserir um novo cadastro na tabela de GR
+async function insertGr(gr){
+    // cria uma variavel para armazenar os valores que serão passados por parametro para o banco de dados cadastrar.
+    const values = [gr.nome, gr.codGr, gr.custo];
+    // armazena no banco de dados as informações do novo usuário cadastrado.
+    await client.query("INSERT INTO grades(nome,codGr,custo) VALUES (?,?,?);", values); 
 }
 
-// função para deletar um cliente no banco de dados
-async function deleteCustomer(id){
-    const value = [id];
-    await client.query("DELETE FROM clientes WHERE id=?",value);
+// função para inserir um novo cadastro na tabela de EQ
+async function insertEq(eq){
+    // cria uma variavel para armazenar os valores que serão passados por parametro para o banco de dados cadastrar.
+    const values = [eq.nome, eq.codProd, eq.custo];
+    // armazena no banco de dados as informações do novo usuário cadastrado.
+    await client.query("INSERT INTO produtofinal(nome,codProd,custo) VALUES (?,?,?);", values); 
 }
 
+// função para inserir um novo item a uma estrutura
+async function insertEstruturaGr(gr){
+    // cria uma variavel para armazenar os valores que serão passados por parametro para o banco de dados cadastrar.
+    const values = [gr.idGr, gr.idMp, gr.qtd];
+    // armazena no banco de dados as informações do novo usuário cadastrado.
+    await client.query("INSERT INTO estruturagr(idGr,idMp,qtd) VALUES (?,?,?);", values); 
+}
+
+// função para inserir um novo item a uma estrutura
+async function insertEstruturaEq(eq){
+    // cria uma variavel para armazenar os valores que serão passados por parametro para o banco de dados cadastrar.
+    const values = [eq.idEq, eq.idGr];
+    // armazena no banco de dados as informações do novo usuário cadastrado.
+    await client.query("INSERT INTO estruturaproduto(idGr,idMp) VALUES (?,?);", values); 
+}
+
+// função para inserir um novo item a uma estrutura
+async function insertTipoProduto(eq){
+    // cria uma variavel para armazenar os valores que serão passados por parametro para o banco de dados cadastrar.
+    const values = [eq.nome];
+    // armazena no banco de dados as informações do novo usuário cadastrado.
+    await client.query("INSERT INTO tipoproduto(nome) VALUES (?);", values); 
+}
+
+// função para inserir um novo item a uma estrutura
+async function insertProdutoNivel(produto){
+    // cria uma variavel para armazenar os valores que serão passados por parametro para o banco de dados cadastrar.
+    const values = [produto.idTipoProduto, produto.nivel1,produto.nivel2,,produto.nivel3,produto.nivel4,produto.nivel5,
+                    produto.nivel6,produto.nivel7,,produto.nivel8,,produto.nivel9,,produto.nive10,];
+    // armazena no banco de dados as informações do novo usuário cadastrado.
+    await client.query("INSERT INTO produtonivel(idtipoproduto, nivel1, nivel2, nivel3, nivel4, nivel5, nivel6, nivel7, nivel8, nivel9, nivel0) VALUES (?,?,?,?,?,?,?,?,?,?,?);", values); 
+}
 
 module.exports = {
-    selectCustomers,
-    selectCustomer,
-    insertCustomer,
-    updateCustomer,
-    deleteCustomer
+    selectMps,
+    selectMp,
+    selectGrs,
+    selectGr,
+    selectEqs,
+    selectEq,
+    selectEstruturaGr,
+    selectEstruturaEq,
+    selectTipoProdutos,
+    selectTipoProduto,
+    selectProdutoNiveis,
+    selectProdutoNivel,
+    insertMp,
+    insertGr,
+    insertEq,
+    insertEstruturaGr,
+    insertEstruturaEq,
+    insertTipoProduto,
+    insertProdutoNivel    
 }
--- index.js
+++ index.js
@@ -10,16 +10,17 @@ const app = express(); // inicializa uma nova aplicação de backend
 app.use(express.json());
 
 // função que mandar deletar um cliente do banco de dados
-app.delete("/clientes/:id",async (request,response) =>{
+/*app.delete("/clientes/:id",async (request,response) =>{
     // converte id de string para inteiro e atribui a id
     const id = parseInt(request.params.id);
     // chama a função que executa o delete dos dados no banco
     await db.deleteCustomer(id);
     // deletado com sucesso
     response.sendStatus(204);
-})
+})*/
+
 // função que atualiza os dados no cadastro dos clientes
-app.patch("/clientes/:id", async (request,response) =>{
+/*app.patch("/clientes/:id", async (request,response) =>{
     // converte id de string para inteiro e atribui a id
     const id = parseInt(request.params.id);
     // atribui a customer a alteração que será executada no cadastro indicado pelo id
@@ -28,31 +29,156 @@ app.patch("/clientes/:id", async (request,response) =>{
     await db.updateCustomer(id, customer);
     // responde ok
     response.sendStatus(200);
+})*/
+
+// função para cadastrar uma nova materia prima
+app.post("/materiaprima",async (request,response) =>{
+    // indica no protocolo http que a informação a ser cadastrada esta no corpo do protocolo
+    const mp = request.body;
+    // rotina assincrona para inserir o novo cliente no banco
+    await db.insertMp(mp);
+    // codigo de resposta global para cadastro com sucesso
+    response.sendStatus(201);
 })
 
-// função para cadastrar um novo cliente
-app.post("/clientes",async (request,response) =>{
+// função para cadastrar uma nova materia prima
+app.post("/grades",async (request,response) =>{
     // indica no protocolo http que a informação a ser cadastrada esta no corpo do protocolo
-    const customer = request.body;
+    const gr = request.body;
     // rotina assincrona para inserir o novo cliente no banco
-    await db.insertCustomer(customer);
+    await db.insertGr(gr);
+    // codigo de resposta global para cadastro com sucesso
+    response.sendStatus(201);
+})
+
+// função para cadastrar uma nova materia prima
+app.post("/produtofinal",async (request,response) =>{
+    // indica no protocolo http que a informação a ser cadastrada esta no corpo do protocolo
+    const eq = request.body;
+    // rotina assincrona para inserir o novo cliente no banco
+    await db.insertEq(eq);
+    // codigo de resposta global para cadastro com sucesso
+    response.sendStatus(201);
+})
+
+// função para cadastrar uma nova materia prima
+app.post("/estruturagr",async (request,response) =>{
+    // indica no protocolo http que a informação a ser cadastrada esta no corpo do protocolo
+    const estruturaGrade = request.body;
+    // rotina assincrona para inserir o novo cliente no banco
+    await db.insertEstruturaGr(estruturaGrade);
+    // codigo de resposta global para cadastro com sucesso
+    response.sendStatus(201);
+})
+
+// função para cadastrar uma nova materia prima
+app.post("/estruturaeq",async (request,response) =>{
+    // indica no protocolo http que a informação a ser cadastrada esta no corpo do protocolo
+    const estruturaEq = request.body;
+    // rotina assincrona para inserir o novo cliente no banco
+    await db.insertEstruturaEq(estruturaEq);
+    // codigo de resposta global para cadastro com sucesso
+    response.sendStatus(201);
+})
+
+// função para cadastrar uma nova materia prima
+app.post("/tipoproduto",async (request,response) =>{
+    // indica no protocolo http que a informação a ser cadastrada esta no corpo do protocolo
+    const novoProduto = request.body;
+    // rotina assincrona para inserir o novo cliente no banco
+    await db.insertTipoProduto(novoProduto);
+    // codigo de resposta global para cadastro com sucesso
+    response.sendStatus(201);
+})
+
+// função para cadastrar uma nova materia prima
+app.post("/produtonivel",async (request,response) =>{
+    // indica no protocolo http que a informação a ser cadastrada esta no corpo do protocolo
+    const novaCaracteristica = request.body;
+    // rotina assincrona para inserir o novo cliente no banco
+    await db.insertTipoProduto(novaCaracteristica);
     // codigo de resposta global para cadastro com sucesso
     response.sendStatus(201);
 })
 
 // função que retorna do banco de dados todos os cadastros
-app.get("/clientes",async (request,response) =>{
-    const results = await db.selectCustomers();
+app.get("/materiaprima",async (request,response) =>{
+    const results = await db.selectMps();
     response.json(results);
 })
 
-// função que retorna do banco de dados somente um usuário listado pelo id
-app.get("/clientes/:id",async (request,response) =>{
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/materiaprima/:id",async (request,response) =>{
     const id = parseInt(request.params.id); 
-    const results = await db.selectCustomer(id);
+    const results = await db.selectMp(id);
     response.json(results);
 })
 
+// função que retorna do banco de dados todos os cadastros
+app.get("/grades",async (request,response) =>{
+    const results = await db.selectGrs();
+    response.json(results);
+})
+
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/grades/:id",async (request,response) =>{
+    const id = parseInt(request.params.id); 
+    const results = await db.selectGr(id);
+    response.json(results);
+})
+
+// função que retorna do banco de dados todos os cadastros
+app.get("/produtofinal",async (request,response) =>{
+    const results = await db.selectEqs();
+    response.json(results);
+})
+
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/produtofinal/:id",async (request,response) =>{
+    const id = parseInt(request.params.id); 
+    const results = await db.selectEq(id);
+    response.json(results);
+})
+
+// função que retorna do banco de dados todos os cadastros
+app.get("/estruturagr/:idGr",async (request,response) =>{
+    const idGr = parseInt(request.params.idGr);
+    const results = await db.selectEstruturaGr(idGr);
+    response.json(results);
+})
+
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/estruturaeq/:idEq",async (request,response) =>{
+    const idEq = parseInt(request.params.idEq); 
+    const results = await db.selectEstruturaEq(idEq);
+    response.json(results);
+})
+
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/tipoproduto",async (request,response) =>{
+    const results = await db.selectTipoProdutos();
+    response.json(results);
+})
+
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/tipoproduto/:id",async (request,response) =>{
+    const id = parseInt(request.params.idEq);
+    const results = await db.selectTipoProduto(id);
+    response.json(results);
+})
+
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/produtonivel",async (request,response) =>{
+    const results = await db.selectProdutoNiveis();
+    response.json(results);
+})
+
+// função que retorna do banco de dados somente um cadastro listado pelo id
+app.get("/produtonivel/:idtipoproduto",async (request,response) =>{
+    const id = parseInt(request.params.idtipoproduto);
+    const results = await db.selectProdutoNiveis(id);
+    response.json(results);
+})
 
 
 
